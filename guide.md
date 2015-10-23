6f20f78c931955bd820ffdac0032fef31fc2147c
Igor Goltsov <riversy@gmail.com>
2015-10-23 23:54:31 +0300

    Add # UI layout is fine now

---
+++ guide.md
@@ -0,0 +1,3 @@
+# User Guide
+
+There's ujser guide that will describe how to use this **Text Analysis** app.  
--- ui.R
+++ ui.R
@@ -4,12 +4,28 @@ shinyUI(
   
   fluidPage(
     navbarPage("Text Analysis",
-      tabPanel("App"),
-      tabPanel("Guide"),
-      mainPanel(
-       tabPanel("App", HTML("I'm app.")),
-       tabPanel("Guide", HTML("I'm guide."))
+
+      tabPanel(
+        "App", 
+        sidebarLayout(
+          sidebarPanel(
+            radioButtons("plotType", "Plot type",
+                         c("Scatter"="p", "Line"="l")
+            )
+          ),
+          mainPanel(
+            plotOutput("plot")
+          )
+        )         
+      ),
+      tabPanel(
+        "Guide", 
+        fluidRow(
+          column(12, 
+            includeMarkdown("guide.md")
+          )
+        )
       )
-    ) 
-  )
+    )
+  ) 
 )
